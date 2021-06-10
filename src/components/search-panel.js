b580a9b9fcc9f4c25721bd66e52e08fb41017f2a
Alexxs <irreligious86@gmail.com>
2021-06-10 22:00:54 +0200

    init bootstrap branch

---
+++ src/components/search-panel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './search-panel.css';
+
+const SearchPanel = () => {
+
+    const searchText = 'Type to search';
+
+    return (
+        <input type="text"
+               className="form-control search-input"
+               placeholder={searchText}/>
+    );
+};
+
+export default SearchPanel;
